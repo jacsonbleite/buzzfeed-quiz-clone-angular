1ff5136619bb330dd0dfdfe48f21c9cb758ee9d4
jacsonbleite <jacsonbleite@gmail.com>
2022-10-19 13:16:48 -0400

    Criando propriedades lógigas e regras do quiz

--- src/app/components/quiz/quiz.component.ts
+++ src/app/components/quiz/quiz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quiz_questions from '../../../assets/data/quiz_questions.json'
 
 @Component({
   selector: 'app-quiz',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizComponent implements OnInit {
 
+  title: string = ""
+  questions: any
+  questionSelected: any
+  answers: string[] = []
+  answerSelected: string = ""
+  questionIndex: number = 0
+  questionMaxindex: number = 0
+  finished: boolean = false
+
   constructor() { }
 
   ngOnInit(): void {
+    if (quiz_questions) {
+      this.finished = false
+      this.title = quiz_questions.title
+      this.questions = quiz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+      this.questionIndex = 0
+      this.questionMaxindex = this.questions.length
+    }
+  }
+
+  playerChoose(value: string) {
+
+    this.answers.push(value)
+    console.log(this.answers)
+
+  }
+
+  async nextStep() {
+
+    this.questionIndex += 1
+    if (this.questionMaxindex > this.questionIndex) {
+      this.questionSelected = this.questions[this.questionIndex]
+    } else {
+      this.finished = true
+    }
+
   }
 
 }
